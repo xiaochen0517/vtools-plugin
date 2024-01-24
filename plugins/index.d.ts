9f9dfd7adbe7b91fd5c20dc6cbb1ab28630ade91
mochenya <2827075398@qq.com>
2024-01-24 10:40:21 +0800

    update

--- plugins/index.d.ts
+++
@@ -1,8 +0,0 @@
-import {VToolsUtils} from "./utils";
-import {ComponentCustomProperties} from "@vue/runtime-core";
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    vtools: VToolsUtils;
-  }
-}
--- plugins/index.ts
+++ plugins/index.ts
@@ -1,5 +1,12 @@
 import {Installer as VTools} from "./installer.ts";
+import {VToolsUtils} from "./utils";
 
 export {StrUtil} from "./utils";
 export default VTools;
 
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    vtools: VToolsUtils;
+  }
+}
